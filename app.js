005ae9cc67d2a72014ce2c35255eb455948251b0
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-06 12:29:53 -0500

    Update database configuration
    Instead of using a single database hard-coded into app.js, we get the
    database URI from the OS environment (if it has been defined).
    Otherwise, use our development database.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Platyphyllum:Ilda1220@cluster0.fevm6.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://Platyphyllum:Ilda1220@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));

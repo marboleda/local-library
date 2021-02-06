903819b467876e2e7f9af38e3b9ec3f80968851c
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-06 12:19:51 -0500

    Add helmet middleware
    Helmet is a library that can set appropriate HTTP headers that help
    protect your app from well-known web vulnerabilities.

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
+
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
